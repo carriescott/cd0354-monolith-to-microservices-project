3261a12756ec699613273aef5cc458b668d57054
Carrie <carriescott0@gmail.com>
2023-06-03 15:13:51 +0100

    Refactor to  microservice and set up docker files

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export POSTGRES_USERNAME=cdr
+export POSTGRES_PASSWORD=Peachy89!
+export POSTGRES_HOST=microservice-827001410269-dev-3.cc62pjeha6fb.eu-west-2.rds.amazonaws.com
+export POSTGRES_DB=microservice_827001410269_dev_3
+export AWS_BUCKET=arn:aws:s3:::microservice-827001410269-dev
+export AWS_REGION=eu-west-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
