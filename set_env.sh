01e5542f6dd43153e97e13c0947e240b23df0500
Carrie <carriescott0@gmail.com>
2023-06-04 13:47:56 +0100

    updates git ignore

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=test
+export POSTGRES_PASSWORD=test
+export POSTGRES_HOST=test
+export POSTGRES_DB=test
+export AWS_BUCKET=test
+export AWS_REGION=test
+export AWS_PROFILE=test
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+
